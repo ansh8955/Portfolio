fae7c4c6181f2f53c4f7fd8b6285acd1d74c8a66
Ansh Upadhyay <upadhyayaansh094@gmail.com>
2024-03-17 22:07:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar.jsx";
+import SocialLinks from "./components/SocialLinks.jsx";
+import Home from "./Home.jsx";
+import About from './components/About.js';
+import Projects from './components/Projects.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Home />
+      <SocialLinks/>
+      <About/>
+      <Projects/>
     </div>
   );
 }
